cb0de09e9ae92883c09637efc05624b16467e656
arshad-qadri <arshadqadri321@gmail.com>
2022-11-11 11:00:55 +0000

    chnages in login header

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -5,17 +5,40 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 const LoginHeader = () => {
-  const location = useLocation()
+  const location = useLocation();
   return (
     <div>
       <Navbar bg="dark" expand="lg">
         <Container>
-          <Link to={"/"} className="text-light nav-link" > <h4>Upload Images</h4> </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light"/>
+          <h4 className="text-light">Upload Images</h4>{" "}
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className="bg-light"
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Link to={"/login"} className="text-light nav-link fw-bold pb-0" style={{borderBottom:location.pathname==="/login"? "2px solid #fff":"none"}} >Login</Link>
-              <Link to={"/register"} className="text-light nav-link fw-bold pb-0" style={{borderBottom:location.pathname==="/register"? "2px solid #fff":"none"}} >Register</Link>
+              <Link
+                to={"/login"}
+                className="text-light nav-link fw-bold pb-0"
+                style={{
+                  borderBottom:
+                    location.pathname === "/login" ? "2px solid #fff" : "none",
+                }}
+              >
+                Login
+              </Link>
+              <Link
+                to={"/register"}
+                className="text-light nav-link fw-bold pb-0"
+                style={{
+                  borderBottom:
+                    location.pathname === "/register"
+                      ? "2px solid #fff"
+                      : "none",
+                }}
+              >
+                Register
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
