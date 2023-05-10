f54a1b5bc435ab5b27f0c1f0f9d748862e55a5bd
arshad-qadri <arshadqadri321@gmail.com>
2023-05-10 07:10:53 +0000

    just capitalized

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,7 @@ import Form from "react-bootstrap/Form";
 import { Button, Col, Row } from "react-bootstrap";
 
 const Login = () => {
-  console.log("process===", process.env.test);
+  console.log("process===", process.env.TEST);
   const nevigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
